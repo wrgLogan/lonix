cb233d784c2f05f5ee24c3f4b3a7f2e6bc13eba8
Logan <wrgLogan@foxmail.com>
2018-08-13 15:33:47 +0800

    init

---
+++ index.js
@@ -0,0 +1,19 @@
+var Lonix = function() {};
+
+Lonix.prototype.request = function(opt) {
+
+}
+
+Lonix.prototype.get = function(url, query, opt) {
+
+}
+
+Lonix.prototype.post = function(url, query, opt) {
+
+}
+
+Lonix.prototype.create = function(opt) {
+
+}
+
+module.exports = new Lonix;
